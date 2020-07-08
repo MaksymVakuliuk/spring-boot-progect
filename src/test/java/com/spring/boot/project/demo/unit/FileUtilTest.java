148244e7b6c97a09676e630983235842a4ce7eab
Maksym Vakuliuk <vakuliukmaksym@gmail.com>
2020-07-08 17:35:18 +0300

    Added test files

--- src/test/java/com/spring/boot/project/demo/unit/FileUtilTest.java
+++ src/test/java/com/spring/boot/project/demo/unit/FileUtilTest.java
@@ -8,13 +8,28 @@ import static org.junit.Assert.assertTrue;
 @SpringBootTest
 public class FileUtilTest {
     private final FileReader fileReader = new FileUtil();
-    private static final String LINES_TEST_FILE_PATH = "/home/max5akul/MateAcademy/test/lines-for-test.txt";
-    private static final String EMPTY_TEST_FILE_PATH = "/home/max5akul/MateAcademy/test/empty-file-test.txt";
+    private static final String LINES_TEST_FILE_PATH
+            = "/home/max5akul/MateAcademy/spring-boot/src/test/resources"
+            + "/tests/fileUtilTest/lines-for-test.txt";
+    private static final String LINES_TEST_CSV_FILE_PATH
+            = "/home/max5akul/MateAcademy/spring-boot/src/test/resources"
+            + "/tests/fileUtilTest/lines-for-test.csv";
+    private static final String EMPTY_TEST_FILE_PATH
+            = "/home/max5akul/MateAcademy/spring-boot/src/test/resources"
+            + "/tests/fileUtilTest/empty-file-test.txt";
 
     @Test
     public void readLines() {
-        String expected = "[I have 4 lines., first line, second line, third line, fourth line.]";
-        assertEquals(expected, fileReader.readLines(LINES_TEST_FILE_PATH).toString());
+        String expectedTxt
+                = "[I have 4 lines., first line, second line, third line, fourth line.]";
+        assertEquals(expectedTxt, fileReader.readLines(LINES_TEST_FILE_PATH).toString());
+        String expectedCsv
+                = "[first_row,second_row,third_row,fourth_road,"
+                + " first_line,first_line,first_line,first_line,"
+                + " second_line,second_line,second_line,second_line,"
+                + " third_line,third_line,third_line,third_line,"
+                + " fourth_line,fourth_line,fourth_line,fourth_line]";
+        assertEquals(expectedCsv, fileReader.readLines(LINES_TEST_CSV_FILE_PATH).toString());
     }
 
     @Test
